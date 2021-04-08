225750561cd4af84461018c267f0f6d828a5b299
HCS Learning <learning@hardcybersoft.cl>
2021-04-08 14:29:24 -0400

    ejemplo basico de Swiper con REACT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import {Swiper, SwiperSlide} from 'swiper/react'
+import SwiperCore, {Navigation, Pagination} from 'swiper'
+import 'swiper/swiper-bundle.css'
+import { useState } from 'react'
+
+SwiperCore.use( [Navigation, Pagination] )
 
 function App() {
+
+  const [imagen, setImagen] = useState('https://picsum.photos/id/1/1000/880')
+
+  const slides = []
+  for(let i=1; i<=10; i++) {
+    (new Image()).src = `https://picsum.photos/id/${i}/1000/880` // preload images
+    slides.push(
+      <SwiperSlide key={`slide-${i}`}>
+        <img src={`https://picsum.photos/id/${i}/200/100`} alt={`Imagen ${i}`} onClick={ (e) => setImagen(`https://picsum.photos/id/${i}/1000/880`)} />
+      </SwiperSlide>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <img src={imagen} alt="imagen-principal" />
+
+      <div style={{width: '1024px'}}>
+        <Swiper
+          spaceBetween={50}
+          slidesPerView={5}
+          slidesPerGroup={5}
+          navigation
+          pagination
+          loop={true}
+          loopFillGroupWithBlank={true}
+          onSlideChange={ s => console.dir(s) }
         >
-          Learn React
-        </a>
-      </header>
+          {slides}
+        </Swiper>
+      </div>
     </div>
   );
 }
